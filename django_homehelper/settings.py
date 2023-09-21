4643f22d42f834ff7cd339e4095fd7d2b3ea863d
andre <aaa.newwork@gmail.com>
2023-09-22 01:26:18 +0300

    some changes in settings

--- django_homehelper/settings.py
+++ django_homehelper/settings.py
@@ -26,10 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = my_django_secret_key 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True                                                                                                                                  
+# DEBUG = False                                                                                                                                 
+# my_debug import from secret.py                                                                                                                
+DEBUG = my_debug
 
-ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1']
+# ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1']
+# my_allowed_hosts import from secret.py
+ALLOWED_HOSTS = my_allowed_hosts
 
 
 # Application definition
@@ -121,6 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
